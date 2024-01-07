0382508e90d2f42e09887c8e0a4c8b2679729da4
eema300 <emmanicolego@gmail.com>
2024-01-06 23:52:26 -0800

    webcam up

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # Machine Learning Exploration
 The purpose of this repository is to document the machine learning topics I have worked with.
 
-## This repository includes projects related to:
+## This repository includes projects related to
 - OpenCV
 - YOLO
 - Computer Vision
+
+### Future
 - Convolutional Neural Networks (CNNs)
 - Tensorflow
 - PyTorch
 - Pandas
-- Numpy
 - Mahotas
--- obj_det_test.py
+++ obj_det_test.py
@@ -1,16 +1,47 @@
-# this is a test file for practicing object detection
+# this is a python project for practicing object detection
 import cv2 as cv
+from ultralytics import YOLO
 
+# define model and
+model = YOLO("yolo-Weights/yolov8n.pt")
+classes = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
 
+# create capture object
 cap = cv.VideoCapture(0)
 
+# catch error in opening camera
 if not cap.isOpened():
     print("error opening camera")
     exit()
 
+# loop to continuously capture frames from webcam
 while True:
-    # capture frame by frame
+    # capture current frame
     ret, frame = cap.read()
+
+    # catch error reading frame
     if not ret:
-        print("cannot read frame–stream may have ended")
-        
+        print("cannot read frame–error or stream may have ended")
+        break
+
+    # send frame to model
+    frame = cv.flip(frame, 1)
+    result = model(frame, stream=True)
+
+    # show frame (this is a video so wait 1ms)
+    cv.imshow("webcam", frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
